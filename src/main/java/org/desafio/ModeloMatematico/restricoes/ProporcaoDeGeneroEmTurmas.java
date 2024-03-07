c22fcf3d0d6aa92b59f6098913eb38deb8a99121
LucasPerini <lucas.perini@unisoma.com>
2024-03-07 13:56:45 -0300

    feat: adiciona arquivos para novas restrições (ainda a serem implementadas)

---
+++ src/main/java/org/desafio/ModeloMatematico/restricoes/ProporcaoDeGeneroEmTurmas.java
@@ -0,0 +1,12 @@
+package org.desafio.ModeloMatematico.restricoes;
+
+/**
+ * v_alocacaoDeHomens
+ * - v_alocacaoDeMulhers
+ * =
+ * v_folga
+ * - v_folgaAux
+ * V (turma)
+ */
+public class ProporcaoDeGeneroEmTurmas {
+}
