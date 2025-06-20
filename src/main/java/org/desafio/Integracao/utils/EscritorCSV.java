44b0a57a0036a7896250d4ac6bcc231e7545b01a
LucasPerini <lucas.perini@unisoma.com>
2025-06-20 20:53:49 -0300

    refactor: altera integrador para trabalhar com encoding windows-1252

--- src/main/java/org/desafio/Integracao/utils/EscritorCSV.java
+++ src/main/java/org/desafio/Integracao/utils/EscritorCSV.java
@@ -4,12 +4,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
 
 public class EscritorCSV {
     public void writeFile(String csvFile, String[] data) {
         try (PrintWriter printWriter = new PrintWriter(
-                new OutputStreamWriter(new FileOutputStream(csvFile), StandardCharsets.UTF_8))) {
+                new OutputStreamWriter(new FileOutputStream(csvFile), "windows-1252"))) {
             for (String line : data) {
                 printWriter.println(line);
             }
--- src/main/java/org/desafio/Integracao/utils/LeitorCSV.java
+++ src/main/java/org/desafio/Integracao/utils/LeitorCSV.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 public class LeitorCSV {
@@ -12,7 +11,7 @@ public class LeitorCSV {
         ArrayList<String> lines = new ArrayList<>();
 
         try (BufferedReader br = new BufferedReader(
-                new InputStreamReader(new FileInputStream(csvPath), StandardCharsets.UTF_8))) {
+                new InputStreamReader(new FileInputStream(csvPath), "windows-1252"))) {
             String line;
             boolean header = true;
 
